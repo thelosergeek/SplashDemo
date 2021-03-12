53a3da9913fb70c29edeaaa3c23ca9507c2bc226
inam <inamkh0001@gmail.com>
2021-03-12 15:01:34 +0530

    Initial commit

---
+++ app/src/main/java/in/example/splashdemo/MainActivity.kt
@@ -0,0 +1,21 @@
+package `in`.example.splashdemo
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        supportActionBar?.hide()
+
+        Handler().postDelayed({
+            val intent = Intent(this,HomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
